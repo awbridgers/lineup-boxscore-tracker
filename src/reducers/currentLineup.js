2ef3f0395ba1bbe3750c5695680c42c74423aac3
awbridgers <awbridgers@users.noreply.github.com>
2019-04-03 11:03:28 -0400

    changes the player reducer to be an array

--- src/reducers/currentLineup.js
+++ src/reducers/currentLineup.js
@@ -1,55 +1,15 @@
 
 
-export const player1 = (state = 'Brandon Childress', action) =>{
+export const currentLineup = (state = ['Brandon Childress','Sharone Wright', 'Chaundee Brown', 'Jaylen Hoard', 'Olivier Sarr'], action) =>{
   switch(action.type){
-    case 'REMOVE_PLAYER_1':
-      return ''
-    case 'ADD_PLAYER_1':
-      return action.name;
-    default:
-      return state;
-  }
-}
-
-export const player2 = (state = 'Sharone Wright', action) =>{
-  switch(action.type){
-    case 'REMOVE_PLAYER_2':
-      return ''
-    case 'ADD_PLAYER_2':
-      return action.name;
-    default:
-      return state;
-  }
-}
-
-export const player3 = (state = 'Chaundee Brown', action) =>{
-  switch(action.type){
-    case 'REMOVE_PLAYER_3':
-      return ''
-    case 'ADD_PLAYER_3':
-      return action.name;
-    default:
-      return state;
-  }
-}
-
-export const player4 = (state = 'Jaylen Hoard', action) =>{
-  switch(action.type){
-    case 'REMOVE_PLAYER_4':
-      return ''
-    case 'ADD_PLAYER_4':
-      return action.name;
-    default:
-      return state;
-  }
-}
-
-export const player5 = (state = 'Olivier Sarr', action) =>{
-  switch(action.type){
-    case 'REMOVE_PLAYER_5':
-      return ''
-    case 'ADD_PLAYER_5':
-      return action.name;
+    case 'REMOVE_PLAYER':
+      let newArray = state.slice();
+      newArray[action.ID] = '';
+      return newArray;
+    case 'ADD_PLAYER':
+      let addPlayerArray = state.slice();
+      addPlayerArray[action.ID] = action.name;
+      return addPlayerArray;
     default:
       return state;
   }
